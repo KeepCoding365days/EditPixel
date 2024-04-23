bf70315523fa89ba7a14319033790c13f308a6be
shaheer-seby <127238883+shaheer-seby@users.noreply.github.com>
2024-04-23 16:01:25 +0500

    added crop module

---
+++ app/src/main/java/com/example/editpixel/HomeScreen.kt
@@ -0,0 +1,219 @@
+package com.example.myapplication
+
+import android.content.ContentValues
+import android.content.Context
+import android.content.Intent
+import android.graphics.Bitmap
+import android.graphics.ImageDecoder
+import android.net.Uri
+import android.os.Build
+import android.os.Environment
+import android.provider.MediaStore
+import android.util.Log
+import android.widget.Toast
+import androidx.activity.compose.rememberLauncherForActivityResult
+import androidx.compose.foundation.Canvas
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.gestures.detectTapGestures
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.aspectRatio
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateListOf
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.setValue
+import androidx.compose.runtime.snapshots.SnapshotStateList
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.clipToBounds
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.ImageBitmap
+import androidx.compose.ui.graphics.Path
+import androidx.compose.ui.graphics.asAndroidBitmap
+import androidx.compose.ui.graphics.asImageBitmap
+import androidx.compose.ui.graphics.drawscope.Stroke
+import androidx.compose.ui.input.pointer.pointerInput
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.IntSize
+import androidx.compose.ui.unit.dp
+import com.canhub.cropper.CropImage.CancelledResult.uriContent
+import com.canhub.cropper.CropImageContract
+import com.canhub.cropper.CropImageContractOptions
+import com.canhub.cropper.CropImageOptions
+import kotlinx.coroutines.launch
+import java.io.IOException
+import java.io.File
+import java.io.OutputStream
+import java.util.LinkedList
+import java.util.Queue
+import kotlinx.coroutines.*
+
+
+@Composable
+fun HomeScreen() {
+    val context = LocalContext.current
+    var bitmap by remember { mutableStateOf<Bitmap?>(null) }
+    var imageUri by remember { mutableStateOf<Uri?>(null) }
+
+    val imageCropLauncher = rememberLauncherForActivityResult(CropImageContract()) {result ->
+        if (result.isSuccessful) {
+            imageUri = result.uriContent
+        }
+        else {
+            val exception = result.error
+        }
+    }
+
+    val polygonCropLauncher = rememberLauncherForActivityResult(CropImageContract()) { result ->
+        if (result.isSuccessful) {
+            imageUri = result.uriContent
+        } else {
+            val exception = result.error
+        }
+    }
+
+    if (imageUri != null) {
+        if (Build.VERSION.SDK_INT < 28) {
+            bitmap = MediaStore.Images.Media.getBitmap(context.contentResolver, imageUri)
+        }
+        else {
+            val source = ImageDecoder.createSource(context.contentResolver, imageUri!!)
+            bitmap = ImageDecoder.decodeBitmap(source)
+        }
+    }
+
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(Color.DarkGray),
+        verticalArrangement = Arrangement.SpaceBetween,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Row(
+            modifier = Modifier
+                .fillMaxHeight(0.1f)
+                .fillMaxWidth()
+                .background(Color.Black)
+                .padding(horizontal = 20.dp),
+            horizontalArrangement = Arrangement.SpaceBetween,
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Button(onClick = { }) {
+                Text("Back")
+            }
+
+            Button(onClick = {
+                 saveBitmapToFile(context, bitmap)
+            }) {
+                Text("Save")
+            }
+        }
+
+        Box(
+            modifier = Modifier
+                .weight(1f)
+                .fillMaxWidth()
+                .background(Color.DarkGray),
+        ) {
+            if (bitmap != null) {
+                Image(
+                    bitmap = bitmap?.asImageBitmap()!!,
+                    contentDescription = null,
+                    contentScale = ContentScale.Crop,
+                    modifier = Modifier.align(Alignment.Center)
+                )
+            }
+        }
+
+        Row(
+            modifier = Modifier
+                .fillMaxHeight(0.1f)
+                .fillMaxWidth()
+                .background(Color.Black)
+                .padding(horizontal = 20.dp),
+            horizontalArrangement = Arrangement.SpaceBetween,
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Button(onClick = {
+                val cropOption = CropImageContractOptions(uriContent, CropImageOptions())
+                imageCropLauncher.launch(cropOption)
+            }) {
+                Text("Select Image and Crop")
+            }
+
+            Button(onClick = {
+                val intent = Intent(context, PolygonCropActivity::class.java)
+                context.startActivity(intent)
+            }) {
+                Text("Polygon Crop")
+            }
+        }
+    }
+}
+
+
+private fun saveBitmapToFile(context: Context, bitmap: Bitmap?) {
+    bitmap?.let { bmp ->
+        val values = ContentValues().apply {
+            put(MediaStore.Images.Media.DISPLAY_NAME, "cropped_image_${System.currentTimeMillis()}.webp")
+            put(MediaStore.Images.Media.MIME_TYPE, "image/webp")
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+                put(MediaStore.Images.Media.RELATIVE_PATH, "${Environment.DIRECTORY_PICTURES}/Stickers")
+            }
+        }
+
+        val uri = context.contentResolver.insert(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, values)
+        if (uri == null) {
+            Log.e("SaveImage", "Failed to create new MediaStore record.")
+            return
+        }
+
+        try {
+            context.contentResolver.openOutputStream(uri).use { outputStream ->
+                if (outputStream == null) {
+                    Log.e("SaveImage", "Failed to get output stream.")
+                    return
+                }
+                if (!bmp.compress(Bitmap.CompressFormat.JPEG, 100, outputStream)) {
+                    Log.e("SaveImage", "Failed to save bitmap.")
+                    return
+                }
+                outputStream?.flush()
+                outputStream?.close()
+            }
+            Toast.makeText(context, "Image saved to gallery", Toast.LENGTH_SHORT).show()
+
+            // Broadcasting to make the image available in the gallery immediately
+            val mediaScanIntent = Intent(Intent.ACTION_MEDIA_SCANNER_SCAN_FILE)
+            mediaScanIntent.data = uri
+            context.sendBroadcast(mediaScanIntent)
+        } catch (e: Exception) {
+            Log.e("SaveImage", "Exception in saving image", e)
+            Toast.makeText(context, "Failed to save image", Toast.LENGTH_SHORT).show()
+        }
+    } ?: run {
+        Toast.makeText(context, "Bitmap is null", Toast.LENGTH_SHORT).show()
+    }
+}
+
+
